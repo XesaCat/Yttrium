2b4bcdb749294ef2cc1565fc1a7b8ed74882946a
XesaCat <neon@xesacat.xyz>
2022-03-13 21:11:57 +0100

    Update OwnerOnly precondition logs when loaded, unloaded and triggered

--- src/preconditions/OwnerOnlyPrecondition.ts
+++ src/preconditions/OwnerOnlyPrecondition.ts
@@ -1,6 +1,10 @@
 import type { PieceContext, PieceOptions, PreconditionResult } from "@sapphire/framework";
+import { Logger } from "../Logger";
 import type { Message } from "discord.js";
 import { Precondition } from "@sapphire/framework";
+import type { TLogLevelName } from "tslog";
+
+const logger = new Logger(process.env.LOGLEVEL as TLogLevelName);
 
 export default class OwnerOnlyPrecondition extends Precondition {
     public constructor(context: PieceContext, options: PieceOptions) {
@@ -10,7 +14,17 @@ export default class OwnerOnlyPrecondition extends Precondition {
         });
     }
 
-    public run(message: Message): PreconditionResult {
+    public override onLoad(): void {
+        logger.debug(`Loaded precondition '${this.name}'`);
+    }
+
+    public override onUnload(): void {
+        logger.debug(`Unloaded precondition '${this.name}'`);
+    }
+
+    public override run(message: Message): PreconditionResult {
+        logger.silly(`Precondition '${this.name}' triggered`);
+
         return process.env.OWNER_IDS?.split(",").includes(message.author.id)
             ? this.ok()
             : this.error({ message: "Only the bot owner can use this command!" });
